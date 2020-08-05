b4709577294d49641e7c1ce8b6cc02e653ed471f
henok Gebremedhn <henoklwi@gmail.com>
2020-08-04 21:29:48 -0700

    generated randome numbers

--- vue-cookies/src/components/HelloWorld.vue
+++ vue-cookies/src/components/HelloWorld.vue
@@ -1,29 +1,32 @@
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm',
-'3pm', '4pm', '5pm', '6pm', '7pm'];
-
 <script>
+let headers = [
+  'location',
+  '6am',
+  '7am',
+  '8am',
+  '9am',
+  '10am',
+  '11am',
+  '12pm',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+  '6pm',
+  '7pm',
+  'total',
+];
+let getRandome = function(min, max) {
+  return Math.ceil(Math.random() * (max - min + 1)) + min;
+};
+
 export default {
   name: 'TheBasics',
   data: () => ({
-    users: [],
-    hours: [
-      'Location',
-      '6am',
-      '7am',
-      '8am',
-      '9am',
-      '10am',
-      '11am',
-      '12pm',
-      '1pm',
-      '2pm',
-      '3pm',
-      '4pm',
-      '5pm',
-      '6pm',
-      '7pm',
-      'total',
-    ],
+    users: ['seattle', 'tokyo', 'dubai', 'paris', 'lima'],
+    headers,
+    getRandome,
   }),
 };
 </script>
@@ -31,21 +34,16 @@ export default {
 <template>
   <v-table :data="users">
     <thead slot="head">
-      <span v-for="item in hours" v-bind:key="item">
+      <span v-for="item in headers" v-bind:key="item">
         <th>{{ item }}</th>
       </span>
     </thead>
 
-    <tbody slot="body" slot-scope="{ displayData }">
-      <tr v-for="row in displayData" :key="row.id">
-        <td>td1</td>
-        <td>td2</td>
-        <td>td3</td>
-        <td>
-          {{ row.address.street }},
-          {{ row.address.city }}
-          {{ row.address.state }}
-        </td>
+    <tbody slot="body">
+      <tr v-for="row in users" :key="row.id">
+        <th>{{ row }}</th>
+
+        <td>{{ getRandome(0, 100) }}</td>
       </tr>
     </tbody>
   </v-table>
