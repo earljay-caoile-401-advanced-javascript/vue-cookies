7df6a9e0efcc9961f757dd26dc7a954ffb84aa63
henok Gebremedhn <henoklwi@gmail.com>
2020-08-05 20:14:01 -0700

    finished salmon cookie

--- vue-cookies/src/main.js
+++ vue-cookies/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import SmartTable from 'vuejs-smart-table';
+import Fragment from 'vue-fragment';
 
+Vue.use(Fragment.Plugin);
 Vue.use(SmartTable);
-
 Vue.config.productionTip = false;
 
 new Vue({
